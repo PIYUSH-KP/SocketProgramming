da39f09f4a8ef131cb338cdf923450d29d05364d
piyushkp <piyush.imp.kp@gmail.com>
2022-09-29 07:13:39 +0530

    Large file transfer

--- largeFileClient.py
+++ largeFileClient.py
@@ -3,7 +3,40 @@ import socket
 from tqdm import tqdm
 
 IP = socket.gethostbyname(socket.gethostname())
-PORT = 5544
+PORT = 5543
 ADDR = (IP, PORT)
 SIZE = 1024
-FILENAME = 
+FORMAT = "utf-8"
+FILENAME = "largefile.txt"
+FILESIZE = os.path.getsize(FILENAME)
+
+
+def main():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(ADDR)
+
+    data = f"{FILENAME}_{FILESIZE}"
+    client.send(data.encode(FORMAT))
+
+    msg = client.recv(SIZE).decode(FORMAT)
+    print(f"SERVER: {msg}")
+
+    bar = tqdm(range(FILESIZE), f"Sending {FILENAME}", unit="B", unit_scale=True, unit_divisor=SIZE)
+
+    with open(FILENAME, "r") as f:
+        while True:
+            data = f.read(SIZE)
+
+            if not data:
+                break
+
+            client.send(data.encode(FORMAT))
+            msg = client.recv(SIZE).decode(FORMAT)
+
+            bar.update(len(data))
+
+        client.close()
+
+
+if __name__ == "__main__":
+    main()
--- largeFileServer.py
+++ largeFileServer.py
@@ -2,7 +2,7 @@ import socket
 from tqdm import tqdm
 
 IP = socket.gethostbyname(socket.gethostname())
-PORT = 5544
+PORT = 5543
 ADDR = (IP,PORT)
 SIZE = 1024
 FORMAT = "utf-8"
@@ -18,6 +18,33 @@ def main():
     conn, addr = server.accept()
     print(f"[+] Client connected from {ADDR[0]}:{ADDR[1]}")
 
+    data = conn.recv(SIZE).decode(FORMAT)
+    item = data.split("_")
+    FILENAME = item[0]
+    FILESIZE = int(item[1])
+    print(f"[+] File {FILENAME} of {FILESIZE} received from client. ")
+    conn.send(f"File {FILENAME} of {FILESIZE} received".encode(FORMAT))
+
+    bar = tqdm(range(FILESIZE), f"Receiving {FILENAME}", unit="B", unit_scale=True, unit_divisor=SIZE)
+
+    with open(f"recv_{FILENAME}", "w") as f:
+        while True:
+            data = conn.recv(SIZE).decode(FORMAT)
+
+            if not data:
+                break
+
+            f.write(data)
+            conn.send("Data received.".encode(FORMAT))
+
+            bar.update(len(data))
+
+        conn.close()
+        server.close()
+
+
+if __name__ == "main":
+    main()
 
 
 
